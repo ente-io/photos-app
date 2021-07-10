82eb7151a343e97e0804432c3bb649a95f081898
Neeraj Gupta <neeraj.gupta983@gmail.com>
2021-07-10 16:05:35 +0530

    Implemented toJson & fromMap for Location model

--- lib/models/location.dart
+++ lib/models/location.dart
@@ -6,4 +6,17 @@ class Location {
 
   @override
   String toString() => 'Location(latitude: $latitude, longitude: $longitude)';
+
+  Map<String, dynamic> toJson() {
+    return {
+      "latitude": this.latitude,
+      "longitude": this.latitude,
+    };
+  }
+
+  factory Location.fromMap(Map<String, dynamic> map) {
+    if (map == null) return null;
+    return Location(
+        double.parse(map['latitude']), double.parse(map['latitude']));
+  }
 }
