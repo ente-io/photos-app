133d1e6ffc1f16a917cf8c6e695f32e263c40d65
ashilkn <ashilkn99@gmail.com>
2022-08-25 21:27:11 +0530

    show hidden albums on hidden screen

---
+++ lib/ui/collections/hidden_collections_list_view_widget.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:photos/models/collection_items.dart';
+import 'package:photos/ui/collections/collection_item_widget.dart';
+import 'package:photos/ui/viewer/gallery/empte_state.dart';
+
+class HiddenCollectionsListViewWidget extends StatelessWidget {
+  final List<CollectionWithThumbnail> hiddenCollections;
+
+  const HiddenCollectionsListViewWidget(
+    this.hiddenCollections, {
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8),
+      child: SizedBox(
+        height: 170,
+        child: Align(
+          alignment: Alignment.centerLeft,
+          child: hiddenCollections.isEmpty
+              ? const EmptyState()
+              : ListView.builder(
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  padding: const EdgeInsets.fromLTRB(6, 0, 6, 0),
+                  physics: const ScrollPhysics(),
+                  // to disable GridView's scrolling
+                  itemBuilder: (context, index) {
+                    return CollectionItem(hiddenCollections[index]);
+                  },
+                  itemCount: hiddenCollections.length,
+                ),
+        ),
+      ),
+    );
+  }
+}
