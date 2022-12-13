aa9cac6dea8c23f54f8a39496325540244c662c3
ashilkn <ashilkn99@gmail.com>
2022-12-13 11:44:07 +0530

    Moved state of numberOfFilesUploaded to the InheritedWidget UserDetailsState

--- lib/states/user_details_state.dart
+++ lib/states/user_details_state.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:photos/core/configuration.dart';
 import 'package:photos/core/event_bus.dart';
+import 'package:photos/db/files_db.dart';
 import 'package:photos/events/opened_settings_event.dart';
 import 'package:photos/events/user_details_changed_event.dart';
 import 'package:photos/models/user_details.dart';
@@ -22,6 +23,7 @@ class UserDetailsStateWidget extends StatefulWidget {
 
 class UserDetailsStateWidgetState extends State<UserDetailsStateWidget> {
   late Future<UserDetails?> userDetails;
+  late Future<int?> numberOfUploadedFiles;
   late StreamSubscription<UserDetailsChangedEvent> _userDetailsChangedEvent;
   late StreamSubscription<OpenedSettingsEvent> _openedSettingsEventSubscription;
 
@@ -31,6 +33,7 @@ class UserDetailsStateWidgetState extends State<UserDetailsStateWidget> {
       _fetchUserDetails();
     } else {
       userDetails = Future.value(null);
+      numberOfUploadedFiles = Future.value(null);
     }
     _userDetailsChangedEvent =
         Bus.instance.on<UserDetailsChangedEvent>().listen((event) {
@@ -59,13 +62,17 @@ class UserDetailsStateWidgetState extends State<UserDetailsStateWidget> {
   Widget build(BuildContext context) => InheritedUserDetails(
         userDetailsState: this,
         userDetails: userDetails,
+        numberOfUploadedFiles: numberOfUploadedFiles,
         child: widget.child,
       );
 
   void _fetchUserDetails() {
-    userDetails = UserService.instance.getUserDetailsV2(memoryCount: false);
     if (mounted) {
-      setState(() {});
+      setState(() {
+        userDetails = UserService.instance.getUserDetailsV2(memoryCount: false);
+        numberOfUploadedFiles = FilesDB.instance
+            .getNumberOfUploadedFiles(Configuration.instance.getUserID());
+      });
     }
   }
 }
@@ -73,12 +80,14 @@ class UserDetailsStateWidgetState extends State<UserDetailsStateWidget> {
 class InheritedUserDetails extends InheritedWidget {
   final UserDetailsStateWidgetState userDetailsState;
   final Future<UserDetails?> userDetails;
+  final Future<int?> numberOfUploadedFiles;
 
   const InheritedUserDetails({
     Key? key,
     required Widget child,
     required this.userDetails,
     required this.userDetailsState,
+    required this.numberOfUploadedFiles,
   }) : super(key: key, child: child);
 
   static InheritedUserDetails? of(BuildContext context) =>
--- lib/ui/settings/settings_title_bar_widget.dart
+++ lib/ui/settings/settings_title_bar_widget.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:logging/logging.dart';
-import 'package:photos/core/configuration.dart';
-import 'package:photos/db/files_db.dart';
+import 'package:photos/states/user_details_state.dart';
 import 'package:photos/theme/ente_theme.dart';
 import 'package:photos/ui/common/loading_widget.dart';
 
@@ -12,43 +11,50 @@ class SettingsTitleBarWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final logger = Logger((SettingsTitleBarWidget).toString());
-    return Container(
-      padding: const EdgeInsets.symmetric(vertical: 4),
-      child: Padding(
-        padding: const EdgeInsets.fromLTRB(8, 0, 20, 0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            IconButton(
-              visualDensity: const VisualDensity(horizontal: -2, vertical: -2),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              icon: const Icon(Icons.keyboard_double_arrow_left_outlined),
-            ),
-            FutureBuilder(
-              future: FilesDB.instance
-                  .getNumberOfUploadedFiles(Configuration.instance.getUserID()),
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  final numberOfUploadedFiles = snapshot.data as int;
-                  return Text(
-                    "${NumberFormat().format(numberOfUploadedFiles)} memories",
-                    style: getEnteTextTheme(context).largeBold,
-                  );
-                } else if (snapshot.hasError) {
-                  logger.severe(
-                    "failed to fetch number of uploaded files from local DB",
-                  );
-                  return const EnteLoadingWidget();
-                } else {
-                  return const EnteLoadingWidget();
-                }
-              },
-            )
-          ],
+
+    final inheritedUserDetails = InheritedUserDetails.of(context);
+    if (inheritedUserDetails == null) {
+      logger.severe((InheritedUserDetails).toString() + " is null");
+      throw Error();
+    } else {
+      return Container(
+        padding: const EdgeInsets.symmetric(vertical: 4),
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(8, 0, 20, 0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              IconButton(
+                visualDensity:
+                    const VisualDensity(horizontal: -2, vertical: -2),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: const Icon(Icons.keyboard_double_arrow_left_outlined),
+              ),
+              FutureBuilder(
+                future: inheritedUserDetails.numberOfUploadedFiles,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    final numberOfUploadedFiles = snapshot.data as int;
+                    return Text(
+                      "${NumberFormat().format(numberOfUploadedFiles)} memories",
+                      style: getEnteTextTheme(context).largeBold,
+                    );
+                  } else if (snapshot.hasError) {
+                    logger.severe(
+                      "failed to fetch number of uploaded files from local DB",
+                    );
+                    return const EnteLoadingWidget();
+                  } else {
+                    return const EnteLoadingWidget();
+                  }
+                },
+              )
+            ],
+          ),
         ),
-      ),
-    );
+      );
+    }
   }
 }
