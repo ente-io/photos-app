e1a25c3913e38baac30311397095488b777a81d5
ashilkn <ashilkn99@gmail.com>
2023-01-04 16:00:08 +0530

    Added logging when fetching 2fa status

--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -735,6 +735,7 @@ class UserService {
 
   Future<bool> fetchTwoFactorStatus() async {
     try {
+      _logger.info("Fetching two factor status");
       final response = await _enteDio.get("/users/two-factor/status");
       setTwoFactor(value: response.data["status"]);
       return response.data["status"];
