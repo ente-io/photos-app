7c0a04f9174e8c091a6c89e78668be557f220eac
laurenspriem <github_uni.fishers@aleeas.com>
2024-01-15 12:25:54 +0100

    Only recalculate cluster suggestions after existing onces are done

--- lib/ui/viewer/people/person_cluster_suggestion.dart
+++ lib/ui/viewer/people/person_cluster_suggestion.dart
@@ -26,6 +26,21 @@ class PersonReviewClusterSuggestion extends StatefulWidget {
 }
 
 class _PersonClustersState extends State<PersonReviewClusterSuggestion> {
+  int currentSuggestionIndex = 0;
+  Key futureBuilderKey = UniqueKey();
+
+  // Declare a variable for the future
+  late Future<List<(int, List<EnteFile>)>> futureClusterSuggestions;
+
+  @override
+  void initState() {
+    super.initState();
+    // Initialize the future in initState
+    _fetchClusterSuggestions();
+    // futureClusterSuggestions = ClusterFeedbackService.instance
+    //     .getClusterFilesForPersonID(widget.person);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,12 +48,12 @@ class _PersonClustersState extends State<PersonReviewClusterSuggestion> {
         title: const Text('Review suggestions'),
       ),
       body: FutureBuilder<List<(int, List<EnteFile>)>>(
-        future: ClusterFeedbackService.instance
-            .getClusterFilesForPersonID(widget.person),
+        key: futureBuilderKey,
+        future: futureClusterSuggestions,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            final List<int> keys = snapshot.data!.map((e) => e.$1).toList();
-            if (keys.isEmpty) {
+            // final List<int> keys = snapshot.data!.map((e) => e.$1).toList();
+            if (snapshot.data!.isEmpty) {
               return Center(
                 child: Text(
                   "No suggestions for ${widget.person.attr.name}",
@@ -46,114 +61,161 @@ class _PersonClustersState extends State<PersonReviewClusterSuggestion> {
                 ),
               );
             }
-            return ListView.builder(
-              itemCount: min(keys.length, 1),
-              itemBuilder: (context, index) {
-                final int clusterID = keys[index];
-                final List<EnteFile> files = snapshot.data![index].$2;
-                return InkWell(
-                  onTap: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => ClusterPage(
-                          files,
-                          personID: widget.person,
-                          cluserID: index,
-                        ),
-                      ),
-                    );
-                  },
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 8.0,
-                      vertical: 20,
-                    ),
-                    child: Column(
-                      key: ValueKey("cluster_id-$clusterID"),
-                      children: <Widget>[
-                        Text(
-                          files.length > 1
-                              ? "These photos belong to ${widget.person.attr.name}?"
-                              : "This photo belongs to ${widget.person.attr.name}?",
-                          style: getEnteTextTheme(context).largeMuted,
-                        ),
-                        const SizedBox(height: 24),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: _buildThumbnailWidgets(
-                            files,
-                            clusterID,
-                          ),
-                        ),
-                        if (files.length > 4) const SizedBox(height: 24),
-                        if (files.length > 4)
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: _buildThumbnailWidgets(
-                              files,
-                              clusterID,
-                              start: 4,
-                            ),
-                          ),
-                        const SizedBox(
-                          height: 24.0,
-                        ),
-                        Text(
-                          "${snapshot.data![index].$2.length} photos",
-                          style: getEnteTextTheme(context).body,
-                        ),
-                        const SizedBox(
-                          height: 24.0,
-                        ), // Add some spacing between the thumbnail and the text
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 24.0),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: <Widget>[
-                              ButtonWidget(
-                                buttonType: ButtonType.primary,
-                                labelText: 'Yes, confirm',
-                                buttonSize: ButtonSize.large,
-                                onTap: () async => {
-                                  await FaceMLDataDB.instance
-                                      .assignClusterToPerson(
-                                    personID: widget.person.remoteID,
-                                    clusterID: clusterID,
-                                  ),
-                                  Bus.instance.fire(PeopleChangedEvent()),
-                                  if (mounted) setState(() => {}),
-                                },
-                              ),
-                              const SizedBox(height: 12.0), // Add some
-                              ButtonWidget(
-                                buttonType: ButtonType.critical,
-                                labelText: 'No',
-                                buttonSize: ButtonSize.large,
-                                onTap: () async => {
-                                  await FaceMLDataDB.instance
-                                      .captureNotPersonFeedback(
-                                    personID: widget.person.remoteID,
-                                    clusterID: clusterID,
-                                  ),
-                                  if (mounted) setState(() => {}),
-                                },
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
+            final numberOfDifferentSuggestions = snapshot.data!.length;
+            final currentSuggestion = snapshot.data![currentSuggestionIndex];
+            final int clusterID = currentSuggestion.$1;
+            final List<EnteFile> files = currentSuggestion.$2;
+            return InkWell(
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => ClusterPage(
+                      files,
+                      personID: widget.person,
+                      cluserID: clusterID,
                     ),
                   ),
                 );
               },
+              child: Container(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 8.0,
+                  vertical: 20,
+                ),
+                child: _buildSuggestionView(
+                  clusterID,
+                  files,
+                  numberOfDifferentSuggestions,
+                ),
+              ),
             );
           } else if (snapshot.hasError) {
+            // log the error
             return const Center(child: Text("Error"));
           } else {
             return const Center(child: CircularProgressIndicator());
           }
         },
       ),
+    );
+  }
+
+  Future<void> _handleUserClusterChoice(
+    int clusterID,
+    bool yesOrNo,
+    int numberOfSuggestions,
+  ) async {
+    // Perform the action based on clusterID, e.g., assignClusterToPerson or captureNotPersonFeedback
+    if (yesOrNo) {
+      await FaceMLDataDB.instance.assignClusterToPerson(
+        personID: widget.person.remoteID,
+        clusterID: clusterID,
+      );
+      Bus.instance.fire(PeopleChangedEvent());
+    } else {
+      await FaceMLDataDB.instance.captureNotPersonFeedback(
+        personID: widget.person.remoteID,
+        clusterID: clusterID,
+      );
+    }
+
+    // Increment the suggestion index
+    if (mounted) {
+      setState(() => currentSuggestionIndex++);
+    }
+
+    // Check if we need to fetch new data
+    if (currentSuggestionIndex >= (numberOfSuggestions)) {
+      setState(() {
+        currentSuggestionIndex = 0;
+        futureBuilderKey = UniqueKey(); // Reset to trigger FutureBuilder
+        _fetchClusterSuggestions();
+      });
+    }
+  }
+
+  // Method to fetch cluster suggestions
+  void _fetchClusterSuggestions() {
+    futureClusterSuggestions = ClusterFeedbackService.instance
+        .getClusterFilesForPersonID(widget.person);
+  }
+
+  Widget _buildSuggestionView(
+    int clusterID,
+    List<EnteFile> files,
+    int numberOfSuggestions,
+  ) {
+    return Column(
+      key: ValueKey("cluster_id-$clusterID"),
+      children: <Widget>[
+        Text(
+          files.length > 1
+              ? "These photos belong to ${widget.person.attr.name}?"
+              : "This photo belongs to ${widget.person.attr.name}?",
+          style: getEnteTextTheme(context).largeMuted,
+        ),
+        const SizedBox(height: 24),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: _buildThumbnailWidgets(
+            files,
+            clusterID,
+          ),
+        ),
+        if (files.length > 4) const SizedBox(height: 24),
+        if (files.length > 4)
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: _buildThumbnailWidgets(
+              files,
+              clusterID,
+              start: 4,
+            ),
+          ),
+        const SizedBox(
+          height: 24.0,
+        ),
+        Text(
+          "${files.length} photos",
+          style: getEnteTextTheme(context).body,
+        ),
+        const SizedBox(
+          height: 24.0,
+        ), // Add some spacing between the thumbnail and the text
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              ButtonWidget(
+                buttonType: ButtonType.primary,
+                labelText: 'Yes, confirm',
+                buttonSize: ButtonSize.large,
+                onTap: () async => {
+                  await _handleUserClusterChoice(
+                    clusterID,
+                    true,
+                    numberOfSuggestions,
+                  ),
+                },
+              ),
+              const SizedBox(height: 12.0), // Add some spacing
+              ButtonWidget(
+                buttonType: ButtonType.critical,
+                labelText: 'No',
+                buttonSize: ButtonSize.large,
+                onTap: () async => {
+                  await _handleUserClusterChoice(
+                    clusterID,
+                    false,
+                    numberOfSuggestions,
+                  ),
+                },
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 
